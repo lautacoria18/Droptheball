a8eac23360f5ff67a1259442b9ddbb945a106f41
Lautaro Coria <lautarocoria97@gmail.com>
2022-04-15 18:55:13 -0300

    Arreglos

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -10,7 +10,7 @@ public class Ball : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        starTaken = false;
     }
 
     // Update is called once per frame
